de9b4f51c64b32c71000d2bce350b7dede1d1bb4
YLG-dev <youenn.legouedec@gmail.com>
2025-06-11 17:00:37 +0200

    wip

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxt/scripts'
+  ]
+})
