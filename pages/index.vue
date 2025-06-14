3471886609aac30a1d715e0e0601f7ca97cea0cf
YLG-dev <youenn.legouedec@gmail.com>
2025-06-14 22:49:12 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,16 @@ const mode = useColorMode()
 <template>
     <div>
         <button class="absolute right-10 top-10" @click="mode = mode === 'dark' ? 'light' : 'dark'">
-            <UIcon v-if="mode === 'dark'" name="uil:sun" size="20" />
-            <UIcon v-else name="uil:moon" size="20" />
+            <!-- <UIcon v-if="mode === 'dark'" name="uil:sun" size="20" /> -->
+            <svg v-if="mode === 'dark'" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                <path fill="currentColor"
+                    d="m5.64 17l-.71.71a1 1 0 0 0 0 1.41a1 1 0 0 0 1.41 0l.71-.71A1 1 0 0 0 5.64 17M5 12a1 1 0 0 0-1-1H3a1 1 0 0 0 0 2h1a1 1 0 0 0 1-1m7-7a1 1 0 0 0 1-1V3a1 1 0 0 0-2 0v1a1 1 0 0 0 1 1M5.64 7.05a1 1 0 0 0 .7.29a1 1 0 0 0 .71-.29a1 1 0 0 0 0-1.41l-.71-.71a1 1 0 0 0-1.41 1.41Zm12 .29a1 1 0 0 0 .7-.29l.71-.71a1 1 0 1 0-1.41-1.41l-.64.71a1 1 0 0 0 0 1.41a1 1 0 0 0 .66.29ZM21 11h-1a1 1 0 0 0 0 2h1a1 1 0 0 0 0-2m-9 8a1 1 0 0 0-1 1v1a1 1 0 0 0 2 0v-1a1 1 0 0 0-1-1m6.36-2A1 1 0 0 0 17 18.36l.71.71a1 1 0 0 0 1.41 0a1 1 0 0 0 0-1.41ZM12 6.5a5.5 5.5 0 1 0 5.5 5.5A5.51 5.51 0 0 0 12 6.5m0 9a3.5 3.5 0 1 1 3.5-3.5a3.5 3.5 0 0 1-3.5 3.5" />
+            </svg>
+            <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                <path fill="currentColor"
+                    d="M21.64 13a1 1 0 0 0-1.05-.14a8.05 8.05 0 0 1-3.37.73a8.15 8.15 0 0 1-8.14-8.1a8.6 8.6 0 0 1 .25-2A1 1 0 0 0 8 2.36a10.14 10.14 0 1 0 14 11.69a1 1 0 0 0-.36-1.05m-9.5 6.69A8.14 8.14 0 0 1 7.08 5.22v.27a10.15 10.15 0 0 0 10.14 10.14a9.8 9.8 0 0 0 2.1-.22a8.11 8.11 0 0 1-7.18 4.32Z" />
+            </svg>
+            <!-- <UIcon name="uil:moon" size="20" /> -->
         </button>
         <div class="mb-10">
             <p><strong>Youenn LE GOUEDEC</strong></p>
