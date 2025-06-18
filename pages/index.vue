7f227cc08ea9bd66d912ccc2d58718fbe1a1af9f
YLG-dev <youenn.legouedec@gmail.com>
2025-06-18 16:20:31 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="space-y-10">
+    <div class="space-y-8">
         <div>
             <p><strong>Youenn LE GOUEDEC</strong></p>
             <p>Freelance</p>
@@ -13,28 +13,28 @@
         <NuxtImg src="/img/photo.jpg" alt="Picture of Me" class="max-w-40 sm:max-w-50 rounded-full" />
         <div>
             <p><strong>Stack</strong></p>
-            <ul>
-                <li>Backend : Laravel + Filament</li>
-                <li>Frontend : Vue + Inertia</li>
-                <li>Environment : Linux + Docker + Caddy</li>
-                <li>Hosting : Hetzner</li>
-
-            </ul>
+            <p>Backend : Laravel + Filament</p>
+            <p>Frontend : Vue + Inertia</p>
+            <p>Environment : Linux + Docker + Caddy</p>
+            <p>Hosting : Hetzner</p>
         </div>
 
         <div>
             <p><strong>About me</strong></p>
-            <ul>
-                <li>Location : Britanny, France</li>
-                <li>Pronouns : He / Him</li>
-                <li>Hobbies : Dancing / African Drums & Accordion / Gardenning </li>
-            </ul>
+            <p>Location : Britanny, France</p>
+            <p>Pronouns : He / Him</p>
+            <p>Hobbies : Dancing / African Drums & Accordion / Gardenning </p>
         </div>
         <div>
             <p><strong>Projects</strong></p>
-            <ul>
-                <li><a href="https://notenn.com">notenn.com</a></li>
-            </ul>
+            <a href="https://notenn.com">notenn.com</a>
+        </div>
+        <div>
+            <p><strong>Comment ça se passe</strong></p>
+            <p>1. Vous m'envoyez un mail avec votre besoin, votre idée, votre projet</p>
+            <p>2. Je vous recontacte pour en discuter</p>
+            <p>3. Je commence à travailler gratuitement</p>
+            <p>4. Vous êtes satisfait, on continue à travailler ensemble et on détermine le coût du projet</p>
         </div>
         <div>
             <p><strong>Contact</strong></p>
@@ -42,6 +42,7 @@
                 <li>youenn@legouedec.fr</li>
             </ul>
         </div>
+
         <a href="https://github.com/y-l-g">
             <UIcon name="uil:github" size="20" />
         </a>
