beb5a4733277e866536d0ea47e95280bdae2abeb
YLG-dev <youenn.legouedec@gmail.com>
2025-07-22 22:57:15 +0200

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,39 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+import tailwindcss from "@tailwindcss/vite";
 
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["./app/assets/css/main.css", "./app/assets/css/markdown.css"],
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/ui'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/ui",
+    "@nuxtjs/i18n",
+  ],
+  i18n: {
+    strategy: "prefix",
+    defaultLocale: "en",
+    locales: [
+      {
+        code: "en",
+        name: "English",
+        file: "en.json",
+      },
+      {
+        code: "fr",
+        name: "Français",
+        file: "fr.json",
+      },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  icon: {
+    clientBundle: {
+      icons: ["uil:moon", "uil:sun"],
+    },
+  },
+});
